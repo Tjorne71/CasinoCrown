77cd4bad51cbe0b75984f57ca4ac23a427657db6
Tjorne71 <emtj@itu.dk>
2023-01-18 10:15:29 +0100

    initial commit

---
+++ head.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Skræddersyet personligt Casino event til din næste fest. Pro Casino borde, udsmykning og uddannede dealere. Få et godt tilbud til din egen casino aften">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <link rel="canonical" href="https://www.casinocrown.dk/">
+ 	<meta name="title" content="Casino Crown | Skræddersyet Casino Event">
+    <meta name="keywords" content="Casino Event, Events, Fest">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="revisit-after" content="7 days">
+    <meta property="og:locale" content="da_DK">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://www.casinocrown.dk/static/icons/logo/BannerFB.png">
+    <meta property="og:title" content="Lej os til at arrangere din næste casino event">
+    <meta property="og:description"
+        content="Skræddersyet personligt Casino event til din næste fest. Pro Casino borde, udsmykning og uddannede dealere. Få et godt tilbud til din egen casino aften">
+    <meta property="og:url" content="https://www.casinocrown.dk/">
+    <meta property="og:site_name" content="Casino Crown">
+
+    <title>Casino Crown | Skræddersyet Casino Event</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="/static/css/main.css">
+    
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-32x32.png">
+    <link rel="manifest" href="static/site.webmanifest">
+    <link rel="mask-icon" href="static/safari-pinned-tab.svg" color="#ffffff">
+    <!-- Font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
+    <script src="static/js/jquery-3.6.0.min.js"></script>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Casino",
+      "name": "Casino Crown",
+      "image": "https://www.casinocrown.dk/static/icons/logo/BannerFB.png",
+      "@id": "https://www.casinocrown.dk",
+      "url": "https://www.casinocrown.dk",
+      "telephone": "+4593941316",
+      "priceRange": "$$",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Svinget 1",
+        "addressLocality": "Allerød",
+        "postalCode": "3450",
+        "addressCountry": "DK"
+      },
+      "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": 55.8705373,
+        "longitude": 12.3498026
+      },
+      "openingHoursSpecification": {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday"
+        ],
+        "opens": "00:00",
+        "closes": "23:59"
+      },
+      "sameAs": [
+        "https://www.facebook.com/Casino-Crown-109456615165354",
+        "https://www.instagram.com/casino_crown/"
+      ] 
+    }
+    </script>
+    <!-- Google tag (gtag.js) --> 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10956053336"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-10956053336'); </script>
+    <!-- Event snippet for Indsend kundeformular conversion page --> 
+    <script> gtag('event', 'conversion', {'send_to': 'AW-10956053336/JkXOCMmplYIYENi2oOgo'}); </script>
+</head>
+
+<body>
+    <div class="contact-form-btn contact-form-btn-border">
+        <div id="contact-form-toggle" class="contact-form-btn">
+            <i class="bi bi-chat-dots"></i>
+        </div>
+    </div>
+    <?php include "contact.html"?>
+    <?php include "nav.html"?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include ("head.php")?>
+<?php include ("firstbox.html" )?>
+<?php include ("secondbox.php" )?>
+<?php include ("thirdbox.html" )?>
+<?php include ("fourthbox.html" )?>
+<script defer src="/static/js/main.js"></script>
+<?php include ("foot.html" )?>
